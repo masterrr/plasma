bfe242d2b5311e6fc2c854d7558a9367e078e7af
Dmitry Kurilo <me@masterrr.name>
2024-12-20 19:27:34 +0000

    plasma

---
+++ plasma/plasma/Rules.swift
@@ -0,0 +1,7 @@
+//
+//  Rules.swift
+//  plasma
+//
+//  Created by Dmitrij Kurilo on 20/12/2024.
+//
+
---
+++ plasma/plasmaTests/plasmaTests.swift
@@ -0,0 +1,17 @@
+//
+//  plasmaTests.swift
+//  plasmaTests
+//
+//  Created by Dmitrij Kurilo on 20/12/2024.
+//
+
+import Testing
+@testable import plasma
+
+struct plasmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
